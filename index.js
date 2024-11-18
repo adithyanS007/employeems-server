c61c02df4f61852755cc5e20b56c5e91c58a9064
adithyanS007 <adhiadithyan21@gmail.com>
2024-11-18 10:52:10 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ import connectToDatabase from './db/db.js'
 
 connectToDatabase()
 const app = express()
-app.use(cors())
+app.use(cors({
+    orgin: "https://localhost//employeems",
+    credentials: true
+}))
 app.use(express.json())
 app.use(express.static('public/uploads'))
 app.use('/api/auth', authRouter)
