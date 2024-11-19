93b4b03daedbf8165216c2c6d955fead153547d2
adithyanS007 <adhiadithyan21@gmail.com>
2024-11-19 15:42:02 +0530

    empController & db files updated

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -14,7 +14,10 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+ });
 
 const addEmployee = async (req, res) => {
     try {
